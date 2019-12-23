e63fb0c38214adb708f16daabd3250819f1f4e03
Dat Nguyen Quoc <dat.nq@liena.local>
2019-12-23 11:11:22 +0700

    abc

--- plugins/magento/index.js
+++ plugins/magento/index.js
@@ -2,7 +2,7 @@ import Admin from './admin'
 import Order from './order'
 import Product from './product'
 import ConfigProduct from './product_configurable'
-import Cart from './Cart'
+import Cart from './cart'
 import rest from './rest'
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
